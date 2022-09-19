24bf1d3eb985e11b424cd9453ce769193067633e
KarthikeyaPatnala12 <82384171+KarthikeyaPatnala12@users.noreply.github.com>
2022-09-20 00:19:15 +0530

    added variables

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+
 import pyautogui as ti 
 import webbrowser as wb
 import time
-#webbrowser.open_new("web.whatsapp.com")
-wb.get('Microsoft Edge')
-edge_path = 
+
+n = int(input("Enter number of messages: "))
+k = input("Enter the name of contact or group: ")
+
+msedge_path=r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+#you can use any browser as you want changing the path
+url="web.whatsapp.com"
+wb.register("msedge",None,wb.BackgroundBrowser(msedge_path))
+wb.get("msedge").open_new_tab(url)
+
 time.sleep(15)
-#Thunder buddies.moveTo(230, 200)
-ti.click(x=230, y=220)
-ti.write('Thunder buddies⚡')
-# pyautogui.keyDown('enter')
+
+ti.click(x=261, y=84)
+ti.write(k)
+
+
 time.sleep(1)
+
 ti.click(x=230, y=350)
 ti.click(x=620, y=950)
 ti.click(x=786, y=955)
-ti.click(x=910, y=730,clicks=30)
+ti.click(x=910, y=730 ,clicks=n)
