0cc779e557e573a52ea8c3d92ea5bb5587c4ea56
KarthikeyaPatnala12 <82384171+KarthikeyaPatnala12@users.noreply.github.com>
2022-09-19 14:09:36 +0530

    added main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui as ti 
+import webbrowser as wb
+import time
+#webbrowser.open_new("web.whatsapp.com")
+wb.get('Microsoft Edge')
+edge_path = 
+time.sleep(15)
+#Thunder buddies.moveTo(230, 200)
+ti.click(x=230, y=220)
+ti.write('Thunder buddies⚡')
+# pyautogui.keyDown('enter')
+time.sleep(1)
+ti.click(x=230, y=350)
+ti.click(x=620, y=950)
+ti.click(x=786, y=955)
+ti.click(x=910, y=730,clicks=30)
