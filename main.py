348506f17cf74a99c8ecfeec0ac7e90ee91d3708
KarthikeyaPatnala12 <82384171+KarthikeyaPatnala12@users.noreply.github.com>
2022-09-24 04:47:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from AppOpener import run
 import pyautogui as ti 
-import webbrowser as wb
 import time
 
 
@@ -11,13 +10,13 @@ run("whatsapp")
 time.sleep(15)
 
 #you need to change your positions according to your sticker positions 
-ti.click(x=478, y=220)
-ti.write(k)
+ti.click(x=478, y=220) # to open search 
+ti.write(k) 
 
-time.sleep(2)
-ti.click(x=314, y=501)
+time.sleep(2) #To load search on whatsapp that needs some time 
+ti.click(x=314, y=501) #Contact or group name 
 
-ti.hotkey('ctrl', 's')
-ti.click(x=1386, y=942 ,clicks=n)
+ti.hotkey('ctrl', 's') #shortcut for sticker panel 
+ti.click(x=1386, y=942 ,clicks=n) 
 
 
